0d7a431818bb6729bee69c6d38f80a9cc350a81f
FELIPE BARROS DE SOUZA SILVA <felipebarros300898@gmail.com>
2020-10-14 21:16:19 -0300

    acresentado arquivo tarefa.servece.php fora do diretorio publico

---
+++ app_lista_tarefas/tarefa.service.php
@@ -0,0 +1,16 @@
+<?php
+
+	class TarefaService {
+		public function inserir() {
+			
+		}
+		public function recuperar() {
+			
+		}
+		public function atualizar() {
+			
+		}
+		public function remover() {
+			
+		}
+	}
