98cd73bca2f1975a9302546da3469d59855c1378
FELIPE BARROS DE SOUZA SILVA <felipebarros300898@gmail.com>
2020-10-15 15:18:29 -0300

    atribuindo valor para atributo

--- app_lista_tarefas/tarefa.controller.php
+++ app_lista_tarefas/tarefa.controller.php
@@ -1,3 +1,11 @@
 <?php
+	$caminho = '../../app_lista_tarefas/';
 
-	print_r($_POST['tarefa']);
+	require $caminho.'tarefa.model.php';
+	require $caminho.'tarefa.service.php';
+	require $caminho.'conexao.php';
+	
+	$tarefa = new Tarefa();
+	$tarefa->__set('tarefa', $_POST['tarefa']);
+	
+	$conexao = new Conexao();
