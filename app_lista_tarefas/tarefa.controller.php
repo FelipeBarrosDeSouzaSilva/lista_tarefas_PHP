1d3654f3c6456e00129c1c80592f971fc5c7de46
FELIPE BARROS DE SOUZA SILVA <felipebarros300898@gmail.com>
2020-11-14 01:22:23 -0200

    tarefa.controller.php: Arquivo privado onde foi inserido uma lógica via get para que em um só script façamos inserção, recuperação e exclusão dos dados. nova_tarefa.php: arquivo htdocs inserção de parametro via GET para funcionamento da lógica contida em tarefa.controller.php. tarefa.controller.php foi adicionado o fechamento da tag php obs nenhuma mudança inerente ao funcionamento do sistema. adicionado uma variável que aciona lógica contida em tarefa.controller.php, a variável foi criada antes para que no require, seja passada o valor e a variável ao arquivo que estásendo chamado.

--- app_lista_tarefas/tarefa.controller.php
+++ app_lista_tarefas/tarefa.controller.php
@@ -1,20 +1,24 @@
 <?php
 	$caminho = '../../app_lista_tarefas/';
-	
 	require $caminho.'tarefa.model.php';
 	require $caminho.'tarefa.service.php';
 	require $caminho.'conexao.php';
 	
-	$tarefa = new Tarefa();
-	$tarefa->__set('tarefa', $_POST['tarefa']);
+	$acao = isset($_GET['acao']) ? $_GET['acao'] : $acao;
 	
-	$conexao = new Conexao();
-	
-	$tarefaService = new TarefaService($conexao, $tarefa);
-	$tarefaService->inserir();
+	if($acao == 'inserir' ) {
+		$tarefa = new Tarefa();
+		$tarefa->__set('tarefa', $_POST['tarefa']);
+		
+		$conexao = new Conexao();
+		
+		$tarefaService = new TarefaService($conexao, $tarefa);
+		$tarefaService->inserir();
 
-	header('Location: nova_tarefa.php?inclusao=1');
-	/*Nos arquivo publico podemos dentro de app_lista_tarefas/nova_tarefa.php
-	  existe uma logica onde verificaomos de o parametro passado via get é inclusao e se inclusao é igual a 1*/
-	
+		header('Location: nova_tarefa.php?inclusao=1');
+		/*Nos arquivo publico podemos dentro de app_lista_tarefas/nova_tarefa.php
+		  existe uma logica onde verificaomos de o parametro passado via get é inclusao e se inclusao é igual a 1*/
+	} else if($acao == 'recuperar') {
+		echo 'Chegamos até aqui graças ao nosso Deus!';
+	}
  
--- htdocs/app_lista_tarefas_public/tarefa.controller.php
+++ htdocs/app_lista_tarefas_public/tarefa.controller.php
@@ -1,3 +1,5 @@
 <?php
 
 	require '../../app_lista_tarefas/tarefa.controller.php';
+
+?>
